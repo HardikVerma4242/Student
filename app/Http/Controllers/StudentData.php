0e0f823b4c76622d145f39be77d3ff42c93217cf
Hardik Verma <hardik.verma@novoinvent.com>
2021-09-21 21:19:29 +0530

    Name change for response

--- app/Http/Controllers/StudentData.php
+++ app/Http/Controllers/StudentData.php
@@ -10,7 +10,7 @@ class StudentData extends Controller
     
     public static function saveStudentData(Request $request){
         if(ModelsStudentData::saveStudentData(array('student_id'=> $request['student_id'],'student_name'=> $request['student_name'],'student_address'=> $request['student_address']))){
-            return "SuccessFully Created";
+            return "SuccessFully Create";
         }
     }
 }
